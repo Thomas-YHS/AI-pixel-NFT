0cd5ebf055646912b34baed6bb3a96fa19a1ca7c
yuehongshu <yue1260151801@163.com>
2025-08-24 12:02:29 +0500

    refactor: 移除Debug Contracts导航菜单
    - 从Header组件中移除Debug Contracts菜单项
    - 清理不再使用的BugAntIcon导入
    - 简化导航菜单，专注于核心NFT功能

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { Bars3Icon, BugAntIcon, PhotoIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, PhotoIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -29,11 +29,6 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "我的收藏",
     href: "/me",
     icon: <PhotoIcon className="h-4 w-4" />,
-  },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
   },
 ];
 
